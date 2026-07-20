7f894b414526442c9e9bd26488f4bb217c4d47c1
petracinmariam <petracinm.ariam@gmail.com>
2026-07-20 18:10:13 +0000

    Add files via upload

---
+++ js/03fgsskryeivh.js
@@ -0,0 +1,32 @@
+
+  $(document).ready(function() {
+   var audioElement = document.createElement('audio');
+   audioElement.setAttribute('src', 'audio/beeep.mp3.mpeg');
+
+   audioElement.addEventListener('ended', function() {
+       this.play();
+   }, false);
+
+
+    $('#mycanvas').click(function() {
+       audioElement.play();
+
+   });
+
+     $('#link_black').click(function() {
+       audioElement.play();
+
+   });
+
+
+      $('.pro_box').click(function() {
+       audioElement.play();
+
+   });
+
+       $('#poptxt').click(function() {
+       audioElement.play();
+
+   });
+
+});
